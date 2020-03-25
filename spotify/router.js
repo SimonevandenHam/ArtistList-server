3dcd03af7dd55fd05277628bf839f27df4a44869
SimonevandenHam <sae.vandenham@gmail.com>
2020-03-25 15:36:47 +0100

    added getValidAccessToken function to always have valid key

--- spotify/router.js
+++ spotify/router.js
@@ -1,5 +1,7 @@
 const SpotifyWebApi = require("spotify-web-api-node");
 const { Router } = require("express");
+const spotifyToken = require("./tokenModel");
+const { getValidAccessToken } = require("./tokenModel");
 
 const router = Router();
 
@@ -27,54 +29,19 @@ router.get("/spotify", async (request, response, next) => {
   }
 });
 
-router.get("/spotify/result", (request, response, next) => {
-  try {
-    console.log("IM I HEREEE?");
-    const credentials = {
-      clientId: `3939a9d8a4f84057a149d1bd72e67e7c`,
-      clientSecret: `0e73ca0c5fe2406bbb983b88ee66cd16`,
-      redirectUri: "http://localhost:4000/spotify/res"
-    };
-
-    const spotifyApi = new SpotifyWebApi(credentials);
-
-    // The code that's returned as a query parameter to the redirect URI
-    const code = `AQBj_MyNecZn1CqMwoedWTiSHyqpfMprQJgXNOJ0lZ8t3Ne840d_cY5I3AKDDVcKywO5diDA7cUd6iAeSla33h9uqg8tA6z4C4Lsu9x_WUEKzeJ_tZR2J-N2v-SZXLnfzMZh61GL97BSXn9l34E_UYBPXLWEciW8mFpxPuhR2I4MTYLX5jMC3hvAaXVgxFoZ7fjSMa15x9cZYmbmSifRzNPtDjgYsTnRKuLV1lY3mQ4bUCuSnHBFrWVHag`;
-
-    // Retrieve an access token and a refresh token
-    spotifyApi.authorizationCodeGrant(code).then(
-      function(data) {
-        console.log("The token expires in " + data.body["expires_in"]);
-        console.log("The access token is " + data.body["access_token"]);
-        console.log("The refresh token is " + data.body["refresh_token"]);
-
-        // Set the access token on the API object to use it in later calls
-        spotifyApi.setAccessToken(data.body["access_token"]);
-        spotifyApi.setRefreshToken(data.body["refresh_token"]);
-      },
-      function(err) {
-        console.log("Something went wrong!", err);
-      }
-    );
-  } catch (error) {
-    next(error);
-  }
-});
-
 //1. zoekvariabele meegeven in url (artiest)
 //2. die variable ook aan de search meegeven
 //3. resultaten vanspoitfy teruggeven aan frontend
-router.get("/spotify/new", async (request, response, next) => {
+router.get("/spotify/artist", async (request, response, next) => {
   try {
-    var spotifyApi = new SpotifyWebApi({
-      accessToken:
-        "BQBtXl_YP4EFGd9YzA_D72Pv2YPyiYpZqxUsvjtSm--vpwBmLXbrEPS4X8Nlr5si2_Ovt_qqpHe2qM4jYSh1AzE-9oSyzHuWRj2yFTLggZWfu6Dh-m7ttN6SmbKj7c3j07Jg0exYKI-w4Y8_3udELtfX0TbGbqOK_MIRiUpIBqXOerVN4w"
+    const accessToken = await getValidAccessToken();
+    console.log("accessToken", accessToken);
+    const spotifyApi = new SpotifyWebApi({
+      accessToken: accessToken
     });
-    // const artist = await spotifyApi.searchTracks("artist:Love")(
-    //   response.send(artist)
-    // );
-    // Do search using the access token
-    spotifyApi.searchArtists("Love").then(
+    //NEXTUP change searchArtitst to input from addEvent
+
+    spotifyApi.searchArtists("arctic monkeys").then(
       function(data) {
         console.log(data.body);
       },
@@ -82,6 +49,8 @@ router.get("/spotify/new", async (request, response, next) => {
         console.log("Something went wrong!", err);
       }
     );
+
+    console.log(accessToken);
   } catch (error) {
     next(error);
   }
---
+++ spotify/tokenModel.js
@@ -0,0 +1,57 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+const moment = require("moment");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const spotifyToken = db.define("spotifyToken", {
+  expires_at: {
+    type: Sequelize.DATE
+  },
+  access_token: {
+    type: Sequelize.TEXT
+  }
+});
+
+const getValidAccessToken = async () => {
+  const token = await spotifyToken.findByPk(1);
+  if (token.expires_at > moment()) {
+    return token.access_token;
+  } else {
+    // Create the api object with the credentials
+    const spotifyApi = new SpotifyWebApi({
+      clientId: `3939a9d8a4f84057a149d1bd72e67e7c`,
+      clientSecret: `0e73ca0c5fe2406bbb983b88ee66cd16`
+    });
+
+    // Retrieve an access token.
+    spotifyApi.clientCredentialsGrant().then(
+      async data => {
+        const updatedToken = {
+          expires_at: moment().add(data.body["expires_in"], "seconds"),
+          access_token: data.body["access_token"]
+        };
+        const updated = await token.update(updatedToken);
+        console.log("The access token expires in " + data.body["expires_in"]);
+        console.log("The access token is " + data.body["access_token"]);
+        return updated.access_token;
+      },
+      function(err) {
+        console.log(
+          "Something went wrong when retrieving an access token",
+          err
+        );
+      }
+    );
+  }
+
+  // controle of expires_at verlopen is (is nu 0) check met moments
+  // nog geldig? return access_token
+
+  // niet meer geldig?
+  // maak call met spotify voor nieuw access token
+  // maak datum + expires_in van spotify
+  // sla die nieuwe datum + access_token op
+  // return access_token
+};
+
+module.exports = { spotifyToken, getValidAccessToken };

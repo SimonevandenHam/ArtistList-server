459d79efd3e7dc4dbab57e71dee5ae9c4a961769
SimonevandenHam <sae.vandenham@gmail.com>
2020-03-26 11:37:22 +0100

    made a addConcert model

---
+++ addConcert/model.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const addConcert = db.define(
+  "concert",
+  {
+    date: {
+      type: Sequelize.DATEONLY,
+      allowNull: false
+    },
+    endDate: {
+      type: Sequelize.DATEONLY,
+      allowNull: true
+    },
+    venue: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    location: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  },
+  {
+    tableName: "concert"
+  }
+);
+module.exports = addConcert;
